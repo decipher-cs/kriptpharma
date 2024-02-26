2bdba9d5e18a7ac78b010288aa2e4fb2a830c654
foobar <uagnl25x@anonaddy.me>
2024-02-26 16:20:36 +0530

    Added usesearchParams

--- src/modules/views/AllProducts.tsx
+++ src/modules/views/AllProducts.tsx
@@ -1,21 +1,17 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import TextField from '../components/TextField'
 import {
     Checkbox,
     Box,
     FormControlLabel,
     FormGroup,
-    Radio,
-    RadioGroup,
     useMediaQuery,
     debounce,
     Accordion,
     AccordionSummary,
     AccordionDetails,
 } from '@mui/material'
-import { Button, Card, CardActions, CardContent, CardHeader, CardMedia } from '@mui/material'
-import Typography from '../components/Typography'
-import { useNavigate, useSearchParams } from 'react-router-dom'
+import { useSearchParams } from 'react-router-dom'
 import medicineInfo from '../../assets/medicine.json'
 import Fuse from 'fuse.js'
 import { ProductList } from '../components/ProductList'
@@ -24,23 +20,18 @@ import { ArrowDownwardRounded } from '@mui/icons-material'
 const fuse = new Fuse(medicineInfo, { keys: ['name'] })
 
 export const AllProducts = () => {
-    const isScreenSmall = useMediaQuery('(max-width: 600px)')
-
-    const [value, setValue] = useState('')
+    const [params, setParams] = useSearchParams()
 
     const [products, setProducts] = useState(medicineInfo)
 
-    const divisions = Array.from(new Set(medicineInfo.map(med => med.division)))
+    const availableFilters = [
+        ...Array.from(new Set(medicineInfo.map(med => med.division))),
+        ...Array.from(new Set(medicineInfo.map(med => med.type))),
+    ]
 
-    const types = Array.from(new Set(medicineInfo.map(med => med.type)))
+    const filterFromParams = params.getAll('filter')
 
-    const filterOptions = [...types, ...divisions]
-
-    const [filter, setFilter] = useState(filterOptions)
-
-    const [params, setParams] = useSearchParams()
-
-    const instantFilter = (val: string, filter: string[]) => {
+    const applyFilter = (val: string, filter: string[]) => {
         const result =
             val.length >= 1
                 ? fuse
@@ -51,47 +42,84 @@ export const AllProducts = () => {
         setProducts(result)
     }
 
-    const debouncedFilter = debounce(instantFilter, 500)
+    const [searchValue, setSearchValue] = useState(params.get('search') ?? '')
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const applyFilterWithDelay = debounce(applyFilter, 800)
+
+    const handleSearchValueChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const val = e.target.value
-        setValue(val)
-        if (val.length > 0) debouncedFilter(val, filter)
+        setSearchValue(val)
+        setParams({ search: val })
+        if (val.length > 0)
+            applyFilterWithDelay(
+                val,
+                appliedFilter.filter(val => val.checked).map(val => val.label),
+            )
+    }
+
+    const [appliedFilter, setAppliedFilter] = useState(
+        availableFilters.map(label => ({
+            label,
+            checked: filterFromParams.length ? filterFromParams.includes(label) : true,
+        })),
+    )
+
+    const handleFilterChange = (index: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
+        let newAppliedFilter = [...appliedFilter]
+        newAppliedFilter[index].checked = event.target.checked
+        setAppliedFilter(newAppliedFilter)
+        const filter = newAppliedFilter.filter(val => val.checked).map(val => val.label)
+        setParams({ filter })
+        applyFilter(searchValue, filter)
     }
 
     return (
         <Box sx={{ p: 3 }}>
-            <TextField placeholder='Search...' helperText='Enter medicine name' value={value} onChange={handleChange} />
+            <Box sx={{ display: 'grid', gap: 3, placeItems: 'center', mb: 3 }}>
+                <TextField
+                    sx={{ width: 'calc(1px + 50%)' }}
+                    placeholder='Search...'
+                    helperText='Enter medicine name'
+                    value={searchValue}
+                    onChange={handleSearchValueChange}
+                />
 
-            <Accordion defaultExpanded sx={{ my: 3 }}>
-                <AccordionSummary expandIcon={<ArrowDownwardRounded />}>Filter</AccordionSummary>
-                <AccordionDetails>
-                    <FormGroup row={!isScreenSmall}>
-                        {filterOptions.map(fil => {
-                            return (
-                                <FormControlLabel
-                                    key={fil}
-                                    label={fil.toLowerCase()}
-                                    control={
-                                        <Checkbox
-                                            defaultChecked
-                                            onChange={(_, checked) => {
-                                                const newFilters = checked
-                                                    ? [...filter, fil]
-                                                    : filter.filter(item => item !== fil)
-                                                setFilter(newFilters)
-                                                instantFilter(value, newFilters)
-                                            }}
-                                        />
-                                    }
-                                />
-                            )
-                        })}
-                    </FormGroup>
-                </AccordionDetails>
-            </Accordion>
+                <Filter checked={appliedFilter} handleChange={handleFilterChange} />
+            </Box>
 
             <ProductList products={products} />
         </Box>
+    )
+}
+
+const Filter = (props: {
+    checked: { label: string; checked: boolean }[]
+    handleChange: (index: number) => (event: React.ChangeEvent<HTMLInputElement>) => void
+}) => {
+    const { checked, handleChange } = props
+
+    const isScreenSmall = useMediaQuery('(max-width: 600px)')
+
+    return (
+        <Accordion sx={{ border: 'solid 2px lightgrey' }} elevation={0}>
+            <AccordionSummary expandIcon={<ArrowDownwardRounded />}>Filter</AccordionSummary>
+            <AccordionDetails>
+                <FormGroup>
+                    {checked.map((state, index) => (
+                        <FormControlLabel
+                            key={state.label}
+                            label={state.label.toLowerCase()}
+                            control={
+                                <Checkbox
+                                    checked={checked[index].checked}
+                                    onChange={handleChange(index)}
+                                    inputProps={{ 'aria-label': state.label }}
+                                />
+                            }
+                        />
+                    ))}
+                </FormGroup>
+            </AccordionDetails>
+        </Accordion>
     )
 }
