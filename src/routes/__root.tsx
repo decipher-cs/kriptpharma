299c8b241c285a7cfe5a019f8ad75caadc20d691
foobar <uagnl25x@anonaddy.me>
2024-03-24 18:26:20 +0530

    added top margin to footer

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,7 +5,7 @@ export const Route = createRootRoute({
     component: () => (
         <>
             <Navbar />
-            <div className="min-h-svh min-w-full">
+            <div className="min-h-svh min-w-full relative">
                 <Outlet />
             </div>
             <Footer />
@@ -51,7 +51,7 @@ const Navbar = () => {
 
 const Footer = () => {
     return (
-        <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
+        <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded mt-14">
             <nav className="grid grid-flow-col gap-4">
                 <a className="link link-hover">About us</a>
                 <a className="link link-hover">Contact</a>
