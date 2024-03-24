ed2bef12d756b439641dfff64d1f56dcdbe38761
foobar <uagnl25x@anonaddy.me>
2024-03-24 19:53:46 +0530

    added map to global presence

--- src/routes/globalPresence.lazy.tsx
+++ src/routes/globalPresence.lazy.tsx
@@ -1,5 +1,5 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
-const map = '../assets/amCharts.png'
+import map from '../assets/World-Map-w-japan.svg'
 
 export const Route = createLazyFileRoute('/globalPresence')({
     component: () => <GlobalPresence />,
@@ -7,22 +7,12 @@ export const Route = createLazyFileRoute('/globalPresence')({
 
 const GlobalPresence = () => {
     return (
-        <>
-            <h2 className="text-5xl text-center">Global Presence</h2>
-            <p className="max-w-2xl mt-10 leading-relaxed mx-auto text-xl">
-                Lorem ipsum dolor sit amet, officia excepteur ex fugiat
-                reprehenderit enim labore culpa sint ad nisi Lorem pariatur
-                mollit ex esse exercitation amet. Nisi anim cupidatat excepteur
-                officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip
-                amet voluptate voluptate dolor minim nulla est proident. Nostrud
-                officia pariatur ut officia. Sit irure elit esse ea nulla sunt
-                ex occaecat reprehenderit commodo officia dolor Lorem duis
-                laboris cupidatat officia voluptate. Culpa proident adipisicing
-                id nulla nisi laboris ex in Lorem sunt duis officia eiusmod.
-                Aliqua reprehenderit commodo ex non excepteur duis sunt velit
-                enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur
-                et est culpa et culpa duis.
-            </p>
-        </>
+        <section className="text-center space-y-8">
+            <h2 className="text-5xl">Global Presence</h2>
+            <img
+                src={map}
+                className="w-4/5 border-2 border-base-300 rounded-xl mx-auto p-4"
+            />
+        </section>
     )
 }
