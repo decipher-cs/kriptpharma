8d0c5e9ca94f8c2a0853d886fcadeffc52f50306
foobar <uagnl25x@anonaddy.me>
2024-04-03 19:00:49 +0530

    moved 404 page to view/

---
+++ src/view/PageNotFount.tsx
@@ -0,0 +1,18 @@
+import { Link } from "@tanstack/react-router"
+
+export const ErrorPage = () => {
+    return (
+        <div className="grid h-screen place-content-center px-4">
+            <div className="space-y-7 text-center">
+                <h1 className="text-9xl font-black">404</h1>
+                <p className="text-2xl font-bold tracking-tight sm:text-4xl">
+                    Page Not Found!
+                </p>
+                <p className="text-base-content">We can't find that page.</p>
+                <Link to={'/'} className="btn btn-primary">
+                    Go Back Home
+                </Link>
+            </div>
+        </div>
+    )
+}
