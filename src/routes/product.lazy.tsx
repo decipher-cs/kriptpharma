42a2eb03f17766b48e0b4c9fde675bb7cac07ab3
foobar <uagnl25x@anonaddy.me>
2024-04-05 21:00:34 +0530

    fixed svg

--- src/routes/product.lazy.tsx
+++ src/routes/product.lazy.tsx
@@ -133,9 +133,9 @@ const Product = () => {
                             viewBox="0 0 24 24"
                             fill="none"
                             stroke="currentColor"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                         >
                             <path d="M3 5v14" />
                             <path d="M21 12H7" />
@@ -186,9 +186,9 @@ const Product = () => {
                             viewBox="0 0 24 24"
                             fill="none"
                             stroke="currentColor"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                         >
                             <path d="m9 6-6 6 6 6" />
                             <path d="M3 12h14" />
