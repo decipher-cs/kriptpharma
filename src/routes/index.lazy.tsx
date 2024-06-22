514c63c8fe66a82777e1b3d03b01f589a42b02f1
foobar <uagnl25x@anonaddy.me>
2024-06-22 14:57:01 +0530

    removed testimonials

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -29,7 +29,7 @@ const Index = () => {
                 </h2>
                 <Mason />
             </section>
-            <Testimonial />
+
             <section className="-mx-breath space-y-12 bg-base-200 py-10 lg:-mx-breath-lg">
                 <h2 className="text-center text-3xl font-bold lg:text-5xl">
                     Why Choose Us?
@@ -119,66 +119,6 @@ const Hero = () => {
                         MORE
                     </a>
                 </div>
-            </div>
-        </section>
-    )
-}
-
-const Testimonial = () => {
-    return (
-        <section className="mx-auto space-y-12">
-            <h2 className="text-center text-3xl font-bold lg:text-5xl">
-                Read trusted reviews from our customers
-            </h2>
-
-            <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-5">
-                {Array(3)
-                    .fill(null)
-                    .map((_, i) => (
-                        <blockquote
-                            key={i}
-                            className="space-y-5 rounded-lg bg-neutral p-6 text-neutral-content shadow-sm md:p-8"
-                        >
-                            <div className="flex items-center gap-5">
-                                <img
-                                    alt=""
-                                    src="https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
-                                    className="size-14 rounded-full object-cover"
-                                />
-
-                                <div>
-                                    <div className="flex justify-center gap-0.5 text-primary">
-                                        <span className="sr-only">5 star</span>
-                                        {Array(4)
-                                            .fill(null)
-                                            .map((d, i) => (
-                                                <svg
-                                                    key={i}
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    className="size-5"
-                                                    viewBox="0 0 20 20"
-                                                    fill="currentColor"
-                                                >
-                                                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                                                </svg>
-                                            ))}
-                                    </div>
-
-                                    <p className="mt-0.5 text-lg font-medium text-neutral-content">
-                                        Paul Starr
-                                    </p>
-                                </div>
-                            </div>
-
-                            <p className="italic">
-                                "The services provided are of very high quality
-                                and service provided very comfortable and
-                                friendly to make the customer feel at home and
-                                want to go back for treatment again, really very
-                                interesting experience and lots of fun "
-                            </p>
-                        </blockquote>
-                    ))}
             </div>
         </section>
     )
