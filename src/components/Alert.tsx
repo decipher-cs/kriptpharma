e8f15db3740be51760e9269b200ff004952cbd9c
foobar <uagnl25x@anonaddy.me>
2024-06-22 18:40:28 +0530

    added Alert component

---
+++ src/components/Alert.tsx
@@ -0,0 +1,34 @@
+import { PropsWithChildren, useState } from 'react'
+
+export const Alert = (props: PropsWithChildren) => {
+    const [showAlert, setShowAlert] = useState(true)
+    return (
+        <div
+            className={
+                (!showAlert && ' hidden ') +
+                ' relative z-50 flex items-center justify-between gap-4 bg-primary px-4 py-3 text-primary-content '
+            }
+        >
+            {props.children}
+
+            <button
+                aria-label="Dismiss"
+                className="btn-primary btn-sm"
+                onClick={() => setShowAlert(false)}
+            >
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="size-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                >
+                    <path
+                        fillRule="evenodd"
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                    />
+                </svg>
+            </button>
+        </div>
+    )
+}
