f98c7535dc0e2e828e55c2e18bdeb65c76b2d1d3
foobar <uagnl25x@anonaddy.me>
2024-06-23 18:23:00 +0530

    Added pdf preview to downloads page

--- src/routes/downloads.lazy.tsx
+++ src/routes/downloads.lazy.tsx
@@ -1,5 +1,39 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
+import furniturePdf from '../assets/furniture-catalogue.pdf'
+import pharmaRangePdf from '../assets/pharma-range.pdf'
+import { useState } from 'react'
 
 export const Route = createLazyFileRoute('/downloads')({
-  component: () => <div>Hello /downloads!</div>
+    component: () => <Download />,
 })
+
+const Download = () => {
+    const [selectedPdf, setSelectedPdf] = useState(furniturePdf)
+    return (
+        <section className="">
+            <div className="flex *:basis-1/2">
+                <article className="border">
+                    <h4>Hospital and Furniture Equipement</h4>
+                    <button
+                        className="btn btn-lg"
+                        onClick={() => setSelectedPdf(furniturePdf)}
+                    >
+                        View
+                    </button>
+                </article>
+
+                <article className="border">
+                    <h4>Pharma Range</h4>
+                    <button
+                        className="btn btn-lg"
+                        onClick={() => setSelectedPdf(pharmaRangePdf)}
+                    >
+                        View
+                    </button>
+                </article>
+            </div>
+
+            <embed src={selectedPdf} height={800} width={'100%'} />
+        </section>
+    )
+}
