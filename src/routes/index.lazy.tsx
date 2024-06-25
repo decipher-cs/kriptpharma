baea94c67166723ab4ee8b84f1760fc38a42fa8e
foobar <uagnl25x@anonaddy.me>
2024-06-25 18:31:44 +0530

    added about-us section

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -42,6 +42,7 @@ const Index = () => {
                     ))}
                 </article>
             </section>
+            <About />
 
             <MissionSection />
         </div>
@@ -121,6 +122,41 @@ const FeaturedProducts = () => {
                 </div>
             </section>
         </Breakout>
+    )
+}
+
+const About = () => {
+    return (
+        <article
+            className="mx-auto flex w-max flex-wrap items-center justify-center gap-16"
+            id="about-us"
+        >
+            <h2 className="text-3xl font-bold lg:text-5xl">About Us</h2>
+            <p className="max-w-prose text-lg/relaxed sm:text-xl/loose">
+                A promise for healthy life Kript pharmaceuticals pvt Ltd was
+                established with the motive of adding values to the million of
+                lives through its best quality products , we are here for the
+                people as we serve in the persuit of their healthy lives . We
+                nurture innovation , offers excellent quality and always
+                stretches to inspire confidence in our customer . Our primary
+                objective is to proved high end medical products. At kript
+                pharmaceuticals, we believe in weaving business ethics in all
+                our endeavours and continually updating and upgrading to deliver
+                a uniform quality that surpasses industry benchmarks .
+                <br /> <br />
+                Kript Pharmaceuticals Private Limited is a Registered Indian
+                Pharma Company engaged in the manufacture and marketing of
+                Pharmaceutical formulations. Located in the largest Pharma Hub
+                of Asia, the Company has made a name for itself for its quality
+                and range of products . Supplying not just pharmaceutical,
+                nutraceuticals but also marking their presence into hospital
+                furniture and equipment services . We are equipping multiple
+                hospitals from scratch with our equipment across the globe .
+                Kript pharmaceuticals is a fast expanding Pharma organization
+                having over 400+ registered brands, and is steadily working to
+                make a presence across the Globe.
+            </p>
+        </article>
     )
 }
 
