16166c8f0dcb656ed1b76b21988753541ea09e58
foobar <uagnl25x@anonaddy.me>
2024-07-03 00:33:12 +0530

    updated global presence with high res svg

--- src/routes/globalPresence.lazy.tsx
+++ src/routes/globalPresence.lazy.tsx
@@ -1,22 +1,17 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
 import map from '../assets/global-presence.svg'
-import useTheme from '../hooks/useTheme'
 
 export const Route = createLazyFileRoute('/globalPresence')({
     component: () => <GlobalPresence />,
 })
 
 const GlobalPresence = () => {
-    const { theme } = useTheme()
     return (
-        <section className="space-y-8 text-center">
-            <h2 className="text-2xl md:text-5xl">Global Presence</h2>
+        <section className="">
             <img
                 src={map}
-                className={
-                    'mx-auto w-full rounded-xl border-2 border-neutral-500 bg-neutral-300 p-4 md:w-4/5' +
-                    (theme === 'light' ? ' invert' : ' invert-0')
-                }
+                className={'mx-auto rounded-xl'}
+                alt="global partners on world map"
             />
         </section>
     )
