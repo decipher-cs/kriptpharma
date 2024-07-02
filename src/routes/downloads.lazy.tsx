82b4197dc292635411f01ee5686e7fa7ffa09eb9
foobar <uagnl25x@anonaddy.me>
2024-07-03 00:20:42 +0530

    Simplified download page

--- src/routes/downloads.lazy.tsx
+++ src/routes/downloads.lazy.tsx
@@ -1,39 +1,35 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
 import furniturePdf from '../assets/furniture-catalogue.pdf'
 import pharmaRangePdf from '../assets/pharma-range.pdf'
-import { useState } from 'react'
 
 export const Route = createLazyFileRoute('/downloads')({
     component: () => <Download />,
 })
 
 const Download = () => {
-    const [selectedPdf, setSelectedPdf] = useState(furniturePdf)
     return (
         <section className="">
-            <div className="flex *:basis-1/2">
-                <article className="border">
-                    <h4>Hospital and Furniture Equipement</h4>
-                    <button
-                        className="btn btn-lg"
-                        onClick={() => setSelectedPdf(furniturePdf)}
-                    >
-                        View
-                    </button>
-                </article>
-
-                <article className="border">
-                    <h4>Pharma Range</h4>
-                    <button
-                        className="btn btn-lg"
-                        onClick={() => setSelectedPdf(pharmaRangePdf)}
-                    >
-                        View
-                    </button>
-                </article>
+            <div className="card mx-auto bg-neutral text-neutral-content">
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">Download Our Catalogue</h2>
+                    <div className="card-actions">
+                        <a
+                            className="btn hover:btn-primary"
+                            href={pharmaRangePdf}
+                            target="_blank"
+                        >
+                            Pharmaceutical Range
+                        </a>
+                        <a
+                            className="btn hover:btn-primary"
+                            href={furniturePdf}
+                            target="_blank"
+                        >
+                            {'Furniture & Equipment'}
+                        </a>
+                    </div>
+                </div>
             </div>
-
-            <embed src={selectedPdf} height={800} width={'100%'} />
         </section>
     )
 }
