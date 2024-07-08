39621c7995b0adb18c5e67ad86f183494b30d643
foobar <uagnl25x@anonaddy.me>
2024-07-08 14:21:36 +0530

    Added images to gallery

--- src/routes/gallery.lazy.tsx
+++ src/routes/gallery.lazy.tsx
@@ -1,9 +1,26 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
 
+const modules = import.meta.glob('../assets/gallery/*.*', {
+    eager: true,
+})
+
+const imagePaths = Object.values(modules).map((module) => {
+    if (typeof module === 'object' && module && 'default' in module) {
+        return module.default as string
+    }
+    return undefined
+})
+
 export const Route = createLazyFileRoute('/gallery')({
     component: () => <Gallery />,
 })
 
 const Gallery = () => {
-    return <section></section>
+    return (
+        <section className="flex flex-col gap-4">
+            {imagePaths.map((path) => (
+                <img className="inline-block" src={path} />
+            ))}
+        </section>
+    )
 }
