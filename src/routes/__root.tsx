182cfe32013ca53c4e2301ea017278dd2e14b9c6
foobar <uagnl25x@anonaddy.me>
2024-07-11 20:14:09 +0530

    Alert moved to bottom of the screen to make room for header on the top

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -18,17 +18,6 @@ export const Route = createRootRoute({
     component: () => (
         <div className="selection:bg-secondary selection:text-secondary-content">
             <ThemeProvider>
-                <Alert
-                    alertFor="wip"
-                    text="This website is a work in progress. Any discrepancy can reported to developer@kriptpharmaceuticals.com"
-                />
-                <Alert
-                    alertFor="pharmaexpo"
-                    text="Medi Vietnam PharmaExpo 2024 Saigon Exhibition 1st - 3rd
-                    August 2024 Booth No. IP08 Saigon exhibition and convention
-                    centre at 799 Mguyeb Van Linh Street, Ho Chi Minah City"
-                />
-
                 <Header />
 
                 {/* "px-breath lg:px-breath-lg" is here to globally create breathing room for the content*/}
@@ -36,6 +25,15 @@ export const Route = createRootRoute({
                 <main className="relative mx-auto mt-8 size-full min-h-svh max-w-12xl px-breath lg:px-breath-lg">
                     <Outlet />
                 </main>
+
+                <aside className="fixed bottom-0 z-50 w-full bg-red-500">
+                    <Alert
+                        alertFor="pharmaexpo"
+                        text="Medi Vietnam PharmaExpo 2024 Saigon Exhibition 1st - 3rd
+                    August 2024 Booth No. IP08 Saigon exhibition and convention
+                    centre at 799 Mguyeb Van Linh Street, Ho Chi Minah City"
+                    />
+                </aside>
 
                 <Footer />
                 {import.meta.env.DEV && <TanStackRouterDevtools />}
