85a518e1764868c08ca6fc23e1401cdbf8c04d5b
foobar <uagnl25x@anonaddy.me>
2024-07-17 10:04:02 +0530

    Feat: changed styling and added correct pdf for items

--- src/routes/downloads.lazy.tsx
+++ src/routes/downloads.lazy.tsx
@@ -1,31 +1,44 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
-import furniturePdf from '../assets/furniture-catalogue.pdf'
-import pharmaRangePdf from '../assets/pharma-range.pdf'
+import furniturePdf from '../assets/downloadable-pdf/furniture-catalogue.pdf'
+import pharmaRangePdf from '../assets/downloadable-pdf/pharma-range.pdf'
+import { memo } from 'react'
 
 export const Route = createLazyFileRoute('/downloads')({
-    component: () => <Download />,
+    component: memo(() => <Download />),
 })
 
 const Download = () => {
     return (
-        <section className="">
-            <div className="card mx-auto bg-neutral text-neutral-content">
-                <div className="card-body items-center text-center">
-                    <h2 className="card-title">Download Our Catalogue</h2>
-                    <div className="card-actions">
+        <section className="flex flex-wrap justify-center gap-8">
+            <div className="card card-compact w-96 bg-base-300">
+                <div className="card-body">
+                    <h2 className="card-title">Pharmaceutical Range</h2>
+                    <p>Check out our latest collection of pharmaceutical products</p>
+                    <div className="card-actions justify-end">
                         <a
-                            className="btn hover:btn-primary"
+                            className="btn"
                             href={pharmaRangePdf}
                             target="_blank"
+                            aria-label="download pharmaceutical range catalogue pdf"
                         >
-                            Pharmaceutical Range
+                            View PDF
                         </a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="card card-compact w-96 bg-base-300">
+                <div className="card-body">
+                    <h2 className="card-title">Furniture and Equipment</h2>
+                    <p>Check out our latest collection of pharmaceutical equipment and furniture</p>
+                    <div className="card-actions justify-end">
                         <a
-                            className="btn hover:btn-primary"
+                            className="btn"
                             href={furniturePdf}
                             target="_blank"
+                            aria-label="download equipment and furniture catalogue pdf"
                         >
-                            {'Furniture & Equipment'}
+                            View PDF
                         </a>
                     </div>
                 </div>
