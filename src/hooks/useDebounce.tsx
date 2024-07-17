2603ecd42f26ed580e5028cafff7bc97156d8818
foobar <uagnl25x@anonaddy.me>
2024-07-17 09:59:10 +0530

    feat: added debounce hook

---
+++ src/hooks/useDebounce.tsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react'
+
+export default function useDebounce<T = unknown>(value: T, delay = 500) {
+    const [debounceValue, setDebounceValue] = useState(value)
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setDebounceValue(value)
+        }, delay)
+
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [value, delay])
+
+    return debounceValue
+}
