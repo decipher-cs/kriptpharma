f5e53c16b0dc94e64b01882d084f94c0104e1d5c
foobar <uagnl25x@anonaddy.me>
2024-07-27 18:59:10 +0530

    Style: added spacing to some of the content

--- src/routes/home.lazy.tsx
+++ src/routes/home.lazy.tsx
@@ -489,7 +489,7 @@ const Specialties = () => {
 
 const CompanyStats = () => {
     return (
-        <Breakout className="flex flex-col divide-solid bg-primary py-5 text-primary-content max-lg:divide-y lg:flex-row lg:divide-x">
+        <Breakout className="flex flex-col gap-8 bg-primary px-breath-md py-5 text-primary-content lg:flex-row">
             {[
                 { icon: <PiUsersThreeThin size={65} />, stat: '100+', title: 'Current Employees' },
                 {
@@ -499,13 +499,16 @@ const CompanyStats = () => {
                 },
                 { icon: <PiHandshakeThin size={65} />, stat: '200+', title: 'Partners' },
             ].map((item, i) => (
-                <div key={i} className="inline-flex basis-1/3 flex-col items-center text-center">
+                <div
+                    key={i}
+                    className={clsx(
+                        'flex basis-1/3 flex-col items-center gap-2 py-3 text-center',
+                        'even:px-5 even:max-lg:border-y even:lg:border-x'
+                    )}
+                >
                     <span> {item.icon} </span>
-                    <h6>
-                        <em className="text-6xl font-bold">{item.stat}</em>
-                        <br />
-                        {item.title}
-                    </h6>
+                    <em className="text-6xl font-bold">{item.stat}</em>
+                    <span>{item.title}</span>
                 </div>
             ))}
         </Breakout>
