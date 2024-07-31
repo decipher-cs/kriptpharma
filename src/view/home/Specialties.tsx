bd8ea426dbed9bfde8896091c1f9772e6a9122c1
decipher-cs <130742861+decipher-cs@users.noreply.github.com>
2024-07-31 09:54:20 +0530

    Update Specialties.tsx: new item added to list

--- src/view/home/Specialties.tsx
+++ src/view/home/Specialties.tsx
@@ -1,11 +1,12 @@
-import { PiCertificateThin, PiPencilRulerThin, PiTruckThin } from 'react-icons/pi'
+import { PiCertificateThin, PiGavelThin, PiPencilRulerThin, PiTruckThin } from 'react-icons/pi'
 
 export const Specialties = () => {
     return (
-        <div className="grid gap-10 lg:grid-cols-3">
+        <div className="flex flex-col justify-center gap-10 py-20 md:flex-row">
             {[
                 { icon: <PiTruckThin size={'100%'} />, title: 'Pharmaceuticals Export & Import' },
                 { icon: <PiPencilRulerThin size={'100%'} />, title: 'Research & Development' },
+                { icon: <PiGavelThin size={'100%'} />, title: 'Government Recognised' },
                 {
                     icon: <PiCertificateThin size={'100%'} />,
                     title: 'WHO-GMP Registered',
@@ -15,7 +16,7 @@ export const Specialties = () => {
                     <span className="flex aspect-square max-w-32 place-content-center place-items-center rounded-full bg-primary p-4 text-primary-content">
                         {item.icon}
                     </span>
-                    <h6> {item.title} </h6>
+                    <h6 className="text-center"> {item.title} </h6>
                 </div>
             ))}
         </div>
